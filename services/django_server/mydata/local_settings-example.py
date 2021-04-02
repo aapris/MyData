ecc18eacca4ef116ea7e6fa20b5b652871b9605c
Aapo Rista <aapris@gmail.com>
2021-04-02 19:07:01 +0300

    Add sample local settings

---
+++ services/django_server/mydata/local_settings-example.py
@@ -0,0 +1,6 @@
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+ALLOWED_HOSTS = ['*']  # TODO: to env
+TIME_ZONE = 'Europe/Helsinki'  # TODO: to env
+FIRST_DAY_OF_WEEK = 1
+MONTH_DAY_FORMAT = 'j F '
